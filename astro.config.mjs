d158f8edb07b0cacbe1f1b1dd43fc32deeb10931
derm <dermot.duncan@ihsmarkit.com>
2025-03-31 16:01:41 +0100

    upgrading to astro 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://coderturtle.io",
-  base: "/",
-  trailingSlash: 'always',
+  trailingSlash: "always"
 });
